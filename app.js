9165c4a9e6533886513e2749e6a2b0b563d49cfa
anoy21 <thienhung.kp2@gmail.com>
2023-05-21 12:05:14 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,95 @@
+// var fruitBasket = [
+//     ["Grapes", 15],
+//     ["Apple", 10],
+//     ["Orange", 8],
+//     ["Banana", 12],
+// ];
+
+// console.log("I have " + fruitBasket[0][0] + " " + fruitBasket[1][1])    
+
+// fruitBasket.push(["Mango", 5]);
+// console.log(fruitBasket)
+
+
+// var removedFruit = fruitBasket.pop()
+// console.log(removedFruit)
+// console.log(fruitBasket)
+
+// var shiftedFruit = fruitBasket.shift()
+// console.log(shiftedFruit)
+// console.log(fruitBasket)
+
+// fruitBasket.unshift(["Strawberry", 20])
+// console.log(fruitBasket)
+
+
+
+// for (var i = 0; i < fruitBasket.length; i++) {
+//     var fruit = fruitBasket[i][0]
+//     var quantity = fruitBasket[i][1]
+//     console.log("I have " + quantity + " " + fruit)
+// }
+
+
+let products = {
+    data: [
+
+        {
+        productName : "Lorem1",
+        price : "1",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem2",
+        price : "2",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem3",
+        price : "3",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem4",
+        price : "4",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem5",
+        price : "5",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem6",
+        price : "6",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem7",
+        price : "7",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        },
+
+
+        {
+        productName : "Lorem8",
+        price : "8",
+        image: "https://media.discordapp.net/attachments/493767235744301070/1109705760453173268/fikutfdyqb1a1.jpg?width=454&height=454"
+        }
+
+
+    ]
+}
+
